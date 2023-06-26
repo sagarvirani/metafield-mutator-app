5ad12101cfaad46deafb3bec800bb6650fd1a66c
sagarvirani <sagarvirani@gmail.com>
2023-06-26 14:24:33 +0530

    mutator app

---
+++ web/frontend/components/ProductsCard.jsx
@@ -0,0 +1,82 @@
+import {useCallback, useState} from "react";
+import { Card, TextContainer, Text, TextField  } from "@shopify/polaris";
+import { Toast } from "@shopify/app-bridge-react";
+import { useTranslation } from "react-i18next";
+import { useAppQuery, useAuthenticatedFetch } from "../hooks";
+
+export function ProductsCard() {
+
+  const [collectionId, setCollectionId] = useState();
+
+  const handleCollectionIdChange = useCallback(
+    (newValue) => setCollectionId(newValue),
+    [],
+  );
+
+  const emptyToastProps = { content: null };
+  const [isLoading, setIsLoading] = useState(true);
+  const [toastProps, setToastProps] = useState(emptyToastProps);
+  const fetch = useAuthenticatedFetch();
+  const { t } = useTranslation();
+  const productsCount = 5;
+
+  const {
+    data,
+    refetch: refetchProductCount,
+    isLoading: isLoadingCount,
+    isRefetching: isRefetchingCount,
+  } = useAppQuery({
+    url: "/api/products/count",
+    reactQueryOptions: {
+      onSuccess: () => {
+        setIsLoading(false);
+      },
+    },
+  });
+
+  const toastMarkup = toastProps.content && !isRefetchingCount && (
+    <Toast {...toastProps} onDismiss={() => setToastProps(emptyToastProps)} />
+  );
+
+  const handlePopulate = async () => {
+    setIsLoading(true);
+    const response = await fetch(`/api/products/create/${collectionId}`);
+
+    if (response.ok) {
+      await refetchProductCount();
+      setToastProps({
+        content: t("ProductsCard.productsCreatedToast", {
+          count: productsCount,
+        }),
+      });
+    } else {
+      setIsLoading(false);
+      setToastProps({
+        content: t("ProductsCard.errorCreatingProductsToast"),
+        error: true,
+      });
+    }
+  };
+
+  return (
+    <>
+      {toastMarkup}
+      <Card
+        title="Hide products from search:"
+        sectioned
+        primaryFooterAction={{
+          content: "Update",
+          onAction: handlePopulate,
+          loading: isLoading,
+        }}
+      >
+          <TextField
+            label="Collection ID"
+            value={collectionId}
+            onChange={handleCollectionIdChange}
+            autoComplete="off"
+          />
+      </Card>
+    </>
+  );
+}
---
+++ web/index.js
@@ -0,0 +1,154 @@
+// @ts-check
+import { join } from "path";
+import { readFileSync } from "fs";
+
+import express from "express";
+import serveStatic from "serve-static";
+
+import shopify from "./shopify.js";
+import productCreator from "./product-creator.js";
+import GDPRWebhookHandlers from "./gdpr.js";
+
+const PORT = parseInt(
+  process.env.BACKEND_PORT || process.env.PORT || "3000",
+  10
+);
+
+const STATIC_PATH =
+  process.env.NODE_ENV === "production"
+    ? `${process.cwd()}/frontend/dist`
+    : `${process.cwd()}/frontend/`;
+
+const app = express();
+
+// Set up Shopify authentication and webhook handling
+app.get(shopify.config.auth.path, shopify.auth.begin());
+app.get(
+  shopify.config.auth.callbackPath,
+  shopify.auth.callback(),
+  shopify.redirectToShopifyOrAppRoot()
+);
+app.post(
+  shopify.config.webhooks.path,
+  shopify.processWebhooks({ webhookHandlers: GDPRWebhookHandlers })
+);
+
+// If you are adding routes outside of the /api path, remember to
+// also add a proxy rule for them in web/frontend/vite.config.js
+
+app.use("/api/*", shopify.validateAuthenticatedSession());
+
+app.use(express.json());
+
+app.get("/api/products/count", async (_req, res) => {
+  const countData = await shopify.api.rest.Product.count({
+    session: res.locals.shopify.session,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  res.status(200).send(countData);
+});
+
+app.get("/api/products/create/:collectionId", async (_req, res) => {
+  try {
+    const client = new shopify.api.clients.Rest({
+      session: res.locals.shopify.session,
+    });
+    
+    let collectionId = _req.params.collectionId;
+    let pathVarialbe = `/collections/${collectionId}/products.json`;
+    const limit = 3;
+    
+    let productData = await client.get({
+      path: pathVarialbe,
+      query: { 
+        limit,
+      }
+    });
+    
+    let productArray = [];
+    let nextPageInfo = null;
+
+    while(true) {
+        
+      const products = productData.body.products;
+      productArray = productArray.concat(products);
+
+      const linkHeader = productData.headers["Link"];
+      console.log("link header=",linkHeader);
+      
+      nextPageInfo = extractPageInfo(linkHeader);
+      console.log("next page url=", nextPageInfo);
+      
+      if (!nextPageInfo) {
+        break;
+      }
+      else {
+        productData = await client.get({
+          path: pathVarialbe,
+          query: { 
+            limit: 2,
+            page_info: nextPageInfo,
+          }
+        });
+      }
+      
+    } 
+
+    // Update the metafield for each product
+    console.log("Updating the following products:");
+    const updatedProducts = await Promise.all(
+      productArray.map(async (product) => {
+        const updatedProduct = await client.post({
+          path: `/products/${product.id}/metafields.json`,
+          data: {
+            metafield: {
+              namespace: "custom",
+              key: "demo_counter",
+              value: "1",
+              type: "number_integer" 
+            },
+          },
+        });
+        console.log("Product ID:",product.id);
+        console.log("Product Title:",product.title);
+        return updatedProduct;
+      })
+    );
+
+    res.status(200).send("success");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Extracts the next page URL from the Link header
+// Extracts the 'page_info' parameter from the Link header
+function extractPageInfo(linkHeader) {
+  if (!linkHeader) {
+    return null;
+  }
+
+  const pageInfoRegex = /page_info=([^&>]+)/;
+  const match = pageInfoRegex.exec(linkHeader);
+  if (match) {
+    return match[1];
+  }
+
+  return null;
+}
+
+
+app.use(shopify.cspHeaders());
+app.use(serveStatic(STATIC_PATH, { index: false }));
+
+app.use("/*", shopify.ensureInstalledOnShop(), async (_req, res, _next) => {
+  return res
+    .status(200)
+    .set("Content-Type", "text/html")
+    .send(readFileSync(join(STATIC_PATH, "index.html")));
+});
+
+app.listen(PORT);
